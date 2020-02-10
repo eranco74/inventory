44b947795a9341d882c213176ac483fe2ea35955
Eran Cohen <eranco@redhat.com>
2020-02-10 12:08:18 +0200

    first commit Created MachineHealth operator and CRD

---
+++ pkg/apis/eranco74/group.go
@@ -0,0 +1,6 @@
+// Package eranco74 contains eranco74 API versions.
+//
+// This file ensures Go source parsers acknowledge the eranco74 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package eranco74
---
+++ pkg/apis/eranco74/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the eranco74 v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=eranco74.redhat
+package v1alpha1
